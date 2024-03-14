ff7cc52b8fea800dcf9574b7d2b831554dc46c7f
Sumanjeet <sumanjeet0012@gmail.com>
2024-03-14 06:08:49 +0530

    routing basics

--- first-laravel-app/routes/web.php
+++ first-laravel-app/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', function () {
+    return view('post');
+});
+
+route::view('/direct_view', 'direct_view');
+
+Route::get('/post/firstpost', function () {
+    return view('firstpost');
+});
